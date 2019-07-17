c3ceeb38c2c8e4c9e2cfe5cbb8d4badd27ac7d44
sneakers-the-rat <JLSaunders987@gmail.com>
2019-07-17 16:11:54 -0700

    data generator

---
+++ keras_generator.py
@@ -0,0 +1,107 @@
+import keras
+import numpy as np
+import pandas as pd
+import os
+
+from librosa.feature import melspectrogram
+from librosa.core import load
+
+from tqdm import trange
+
+def load_emotify(data_path):
+    data = pd.read_csv(data_path)
+
+    # drop columns
+    data.drop(axis=1, columns=[' mood', ' liked', ' disliked', ' age', ' gender', ' mother tongue'], inplace=True)
+
+    data.rename(mapper={'track id': 'track_id',
+                        ' genre': 'genre',
+                        ' amazement': 'amazement',
+                        ' solemnity': 'solemnity',
+                        ' tenderness': 'tenderness',
+                        ' nostalgia': 'nostalgia',
+                        ' calmness': 'calmness',
+                        ' power': 'power',
+                        ' joyful_activation': 'joy',
+                        ' tension': 'tension',
+                        ' sadness': 'sadness'},
+                axis=1, inplace=True)
+
+    data.loc[:, 'path'] = ""
+    inputs = []
+    outputs = []
+    for i in trange(data.shape[0]):
+        if (i % 100) == 0:
+            data.loc[i, 'path'] = os.path.join(os.path.split(data_path)[0], 'emotifymusic', data.loc[i, 'genre'],
+                                               "100.mp3")
+        else:
+            data.loc[i, 'path'] = os.path.join(os.path.split(data_path)[0], 'emotifymusic', data.loc[i, 'genre'],
+                                            str(data.loc[i, 'track_id'] % 100) + '.mp3')
+
+    return data
+
+
+class DataGenerator(keras.utils.Sequence):
+    'Generates data for Keras'
+    def __init__(self, song_df, batch_size=32, dim=(64,100), n_channels=1,
+                 n_classes=9, shuffle=True):
+        'Initialization'
+        self.dim = dim
+        self.batch_size = batch_size
+        self.song_df = song_df.reset_index(drop=True)
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.shuffle = shuffle
+        self.audio = {}
+        self.window = 100 # size of audio subsamples
+        self.step_size = 20 # number of steps to take between subsamples
+        self.on_epoch_end()
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        # FIXME: SLOPPY HACK - HARDCODING NUMBER OF SUBSAMPLES PER TRACK
+        n_subsamples = 28
+        return int(np.floor((self.song_df.shape[0]*n_subsamples) / self.batch_size))
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        if index > (self.song_df.shape[0]-self.batch_size):
+            index = index % self.song_df.shape[0]
+
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        # Find list of IDs
+        list_IDs_temp = self.song_df.iloc[indexes,:]
+
+        # Generate data
+        X, y = self.__data_generation(list_IDs_temp)
+
+        return X, y
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(self.song_df.shape[0])
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, list_IDs_temp):
+        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
+        # Initialization
+        X = np.empty((self.batch_size, *self.dim, self.n_channels))
+        Y = np.empty((self.batch_size, self.n_classes), dtype=np.bool)
+
+        # Generate data
+        for i, row in list_IDs_temp.iterrows():
+            if row.path not in self.audio.keys():
+                aud, fs = load(row.path)
+                coefs = melspectrogram(aud, sr=fs, n_fft=2 ** 12, hop_length=2 ** 11,
+                                       n_mels=64, fmax=10000)
+                self.audio[row.path] = coefs
+
+            start_ind = np.random.randint(low=0,high=self.audio[row.path].shape[1]-self.window)
+            clip = self.audio[row.path][:,start_ind+self.window]
+            X[i,:,:,0] = clip
+            Y[i,:] = row.iloc[2:-1].values.astype(np.int64)
+
+        return X, Y
--- keras_mv.py
+++ keras_mv.py
@@ -1,6 +1,6 @@
 import keras
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Conv2D
+from keras.layers import Dense, Activation, Conv2D, Flatten
 
 import os
 import pandas as pd
@@ -8,69 +8,46 @@ from tqdm import tqdm
 
 from librosa.feature import melspectrogram
 from librosa.core import load
+from keras_generator import load_emotify, DataGenerator
 
 import numpy as np
 
 data_dir = '/Users/jonny/Desktop/valence_data'
-data_file = os.path.join(data_dir, 'data_trim.csv')
-data = pd.read_csv(data_file)
+data_file = os.path.join(data_dir, 'data.csv')
+data = load_emotify(data_file)
 
-# drop columns
-data.drop(axis=1, columns=[' mood', ' liked', ' disliked', ' age', ' gender', ' mother tongue'], inplace=True)
+# split into training and testing
+percent_test = 0.05
 
-data.rename(mapper={'track id': 'track_id',
-                    ' genre': 'genre',
-                    ' amazement': 'amazement',
-                    ' solemnity': 'solemnity',
-                    ' tenderness': 'tenderness',
-                    ' nostalgia': 'nostalgia',
-                    ' calmness': 'calmness',
-                    ' power': 'power',
-                    ' joyful_activation': 'joy',
-                    ' tension': 'tension',
-                    ' sadness': 'sadness'},
-            axis=1, inplace=True)
+unique_songs = data.path.unique()
+test_songs = np.random.choice(unique_songs, round(len(unique_songs)*percent_test))
+train_songs = unique_songs[np.invert(np.isin(unique_songs, test_songs))]
 
-data.loc[:, 'path'] = ""
-inputs = []
-outputs = []
-for i in range(data.shape[0]):
-    data.loc[i, 'path'] = os.path.join(data_dir, 'emotifymusic', data.loc[i, 'genre'],
-                                       str(data.loc[i, 'track_id']) + '.mp3')
-
-for i, row in tqdm(data.iterrows()):
-    aud, fs = load(row.path)
-
-    # get a chromagram from the audio
-    coefs = melspectrogram(aud, sr=fs, n_fft=2 ** 12, hop_length=2 ** 11,
-                           n_mels=64, fmax=10000)
-
-    window = 100
-    step_size = 20
-    for k in range(0, coefs.shape[1] - window, step_size):
-        clip = coefs[:, k:k + window]
-        inputs.append(clip)
-        outputs.append(row.iloc[2:-1].values.astype(np.int64))
-
+test_set = data.loc[np.isin(data.path, test_songs),:]
+train_set = data.loc[np.isin(data.path, train_songs),:]
 
 ################
 
+# make generators
+train_generator = DataGenerator(train_set)
+test_generator = DataGenerator(test_set)
+
+
 model = Sequential()
-model.add(Dense(128, input_dim=100))
-model.add(Dense(64))
+model.add(Conv2D(128, kernel_size=(3,3), input_shape=(64,100, 1)))
+model.add(Conv2D(64, kernel_size=(3,3)))
+model.add(Flatten())
 model.add(Dense(32))
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(9, activation='sigmoid'))
 
 model.compile(optimizer="adagrad",
-              loss='categorical_crossentropy',
+              loss='binary_crossentropy',
               metrics=['accuracy'])
 
-X = np.row_stack(inputs)
-Y = np.row_stack(outputs)
-
-data = np.random.random((1000, 100))
-labels = np.random.randint(2, size=(1000, 1))
-model.fit(data, labels)
+model.fit_generator(generator=train_generator,
+                    validation_data=test_generator,
+                    use_multiprocessing=False)
+                    #workers=6)
 
 
 
